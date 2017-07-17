03252cbe9c50c79dbaf49b44e383e0002661d797
Patrick Fiset <patrick.fiset@gmail.com>
2017-07-17 10:53:22 -0400

    travis adam 2

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,9 +1,10 @@
 <?php
+require_once(__DIR__.'/../vendor/autoload.php');
 
 /**
  * @coversDefaultClass \Dracony\skeleton\Hello
  */
-class HelloTest extends \PHPUnit_Framework_TestCase  //marche local, mais pas remote
+class HelloTest extends \PHPUnit_Framework_TestCase
 //class HelloTest extends \PHPUnit\Framework\TestCase
 {
     protected $hello;
