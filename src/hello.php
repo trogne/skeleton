19f3dc8363f64058a0d9a8d6449a9e249d44a9b7
Patrick Fiset <patrick.fiset@gmail.com>
2017-07-17 11:20:41 -0400

    travis adam 5

--- src/hello.php
+++ src/hello.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dracony\skeleton;
+namespace Dracony\Skeleton;
 
 class Hello
 {
--- tests/phpunit.php
+++ tests/phpunit.php
@@ -1,6 +1,5 @@
 <?php
-//echo "max " . __DIR__ . " mix";
-//require_once(__DIR__.'/../vendor/autoload.php');
-require_once('/../vendor/autoload.php');
+
+require_once(__DIR__.'/../vendor/autoload.php');
 
 
