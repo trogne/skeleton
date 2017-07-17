eb2c4de0a446b33c544f41a6764cfe2e37bcf18a
Patrick Fiset <patrick.fiset@gmail.com>
2017-07-17 11:05:52 -0400

    travis adam 3

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -10,7 +10,7 @@ class HelloTest extends \PHPUnit_Framework_TestCase
     
     public function setUp()
     {
-        //$this->hello = new \Dracony\skeleton\Hello();
+        //$this->hello = new \Dracony\Skeleton\Hello();
     }
     
     /**
@@ -18,7 +18,7 @@ class HelloTest extends \PHPUnit_Framework_TestCase
      */
     public function testWorld()
     {
-        $this->hello = new \Dracony\skeleton\Hello();
+        $this->hello = new \Dracony\Skeleton\Hello();
         $this->assertSame('world', $this->hello->world());
     }
 }
