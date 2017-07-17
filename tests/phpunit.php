161f7e8f4a0b700e14c1a94f3fee29a9a61c6694
Patrick Fiset <patrick.fiset@gmail.com>
2017-07-17 11:16:29 -0400

    travis adam 5

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -1,5 +1,6 @@
 <?php
-
-require_once(__DIR__.'/../vendor/autoload.php');
+//echo "max " . __DIR__ . " mix";
+//require_once(__DIR__.'/../vendor/autoload.php');
+require_once('/../vendor/autoload.php');
 
 
