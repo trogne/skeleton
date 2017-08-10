29a3a701a2f159d558dfd656b296c0bb16136d3e
Patrick Fiset <patrick.fiset@gmail.com>
2017-08-10 15:23:08 -0400

    allo

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -5,7 +5,7 @@ require_once(__DIR__.'/../vendor/autoload.php');
  * @coversDefaultClass \Dracony\Skeleton\Hello
  */
 class HelloTest extends \PHPUnit_Framework_TestCase
-//class HelloTest extends \PHPUnit\Framework\TestCase
+//class HelloTest extends \PHPUnit\Framework\TestCase  //for php 7.1
 {
     protected $hello;
     
