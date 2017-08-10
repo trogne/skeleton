88e49e0160afd09fb9cf5e372d128cad12a65a5b
Patrick Fiset <patrick.fiset@gmail.com>
2017-08-10 14:32:03 -0400

    allo

--- tests/phpunit.php
+++ tests/phpunit.php
@@ -1,4 +1,3 @@
 <?php
 
-//require_once(__DIR__.'/../vendor/autoload.php');
-require_once('/home/travis/build/trogne/skeleton/vendor/autoload.php');
+require_once(__DIR__.'/../vendor/autoload.php');
